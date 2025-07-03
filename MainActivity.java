c3095dea84d7c151464b8585fa804d8b3f6d536d
Khubaib Husain <khubaibhusain45@gmail.com>
2025-07-03 06:46:46 +0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -27,41 +27,57 @@ public class MainActivity extends AppCompatActivity {
         heightFeet = findViewById(R.id.normalHeight);
         weights = findViewById(R.id.weight);
         BMIValue = findViewById(R.id.BMIValue);
-
         calculateBMI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int weight = Integer.parseInt(weights.getText().toString());
-                int heightF = Integer.parseInt(heightFeet.getText().toString());
-                int heightI = Integer.parseInt(heightInch.getText().toString());
+                try {
+                    int weight = Integer.parseInt(weights.getText().toString().trim());
+                    int heightF = Integer.parseInt(heightFeet.getText().toString().trim());
+                    int heightI = Integer.parseInt(heightInch.getText().toString().trim());
 
-                int totalInch = heightF * 12 + heightI;
-                double totalCm = totalInch * 2.54;
-                double totalM = totalCm / 100;
+                    int totalInch = heightF * 12 + heightI;
+                    double totalCm = totalInch * 2.54;
+                    double totalM = totalCm / 100;
 
-                double BMI = weight / (totalM * totalM);
-                String formattedBMI = String.format("%.2f", BMI);
-                BMIValue.setText("Your BMI is " + formattedBMI);
+                    double BMI = weight / (totalM * totalM);
+                    String formattedBMI = String.format("%.2f", BMI);
+                    BMIValue.setText("Your BMI is " + formattedBMI);
 
-                if (BMI < 16) {
-                    result.setText("You are Severely Underweight");
-                } else if (BMI >= 16 && BMI <= 16.9) {
-                    result.setText("You are Moderately Underweight");
-                } else if (BMI >= 17 && BMI <= 18.4) {
-                    result.setText("You are Mildly Underweight");
-                } else if (BMI >= 18.5 && BMI <= 24.9) {
-                    result.setText("You are Normal Weighted");
-                } else if (BMI >= 25 && BMI <= 29.9) {
-                    result.setText("You are Overweighted");
-                } else if (BMI >= 30 && BMI <= 34.9) {
-                    result.setText("You are Obese (Class I)");
-                } else if (BMI >= 35 && BMI <= 39.9) {
-                    result.setText("You are Obese (Class II)");
-                } else if (BMI >= 40) {
-                    result.setText("You are Severely Obese (Class III) and must lose weight");
+
+                    double minNormalWeight = 18.5 * totalM * totalM;
+                    double maxNormalWeight = 24.9 * totalM * totalM;
+
+                    if (BMI < 16) {
+                        result.setText("You are Severely Underweight. You should gain at least "
+                                + String.format("%.2f", (minNormalWeight - weight)) + " kg to reach normal weight.");
+                    } else if (BMI >= 16 && BMI <= 16.9) {
+                        result.setText("You are Moderately Underweight. You should gain at least "
+                                + String.format("%.2f", (minNormalWeight - weight)) + " kg to reach normal weight.");
+                    } else if (BMI >= 17 && BMI <= 18.4) {
+                        result.setText("You are Mildly Underweight. You should gain at least "
+                                + String.format("%.2f", (minNormalWeight - weight)) + " kg to reach normal weight.");
+                    } else if (BMI >= 18.5 && BMI <= 24.9) {
+                        result.setText("You are Normal Weighted. Keep up the good work!");
+                    } else if (BMI >= 25 && BMI <= 29.9) {
+                        result.setText("You are Overweighted. You should lose at least "
+                                + String.format("%.2f", (weight - maxNormalWeight)) + " kg to reach normal weight.");
+                    } else if (BMI >= 30 && BMI <= 34.9) {
+                        result.setText("You are Obese (Class I). You should lose at least "
+                                + String.format("%.2f", (weight - maxNormalWeight)) + " kg to reach normal weight.");
+                    } else if (BMI >= 35 && BMI <= 39.9) {
+                        result.setText("You are Obese (Class II). You should lose at least "
+                                + String.format("%.2f", (weight - maxNormalWeight)) + " kg to reach normal weight.");
+                    } else if (BMI >= 40) {
+                        result.setText("You are Severely Obese (Class III) and must lose weight. Aim to lose at least "
+                                + String.format("%.2f", (weight - maxNormalWeight)) + " kg.");
+                    }
+
+                } catch (NumberFormatException e) {
+                    result.setText("Please enter all values correctly.");
+                    BMIValue.setText("");
                 }
-
             }
         });
+
     }
 }
